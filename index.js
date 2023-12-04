5f3c5e160e5a722b70ad4912672b3af237bdc0ba
Bagus Nurhuda <bagus.nurhuda@ui.ac.id>
2023-12-04 17:10:45 +0700

    v7

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", async (req, res) => {
   try {
     res.status(200).send({
       message: "Welcome to my API server!",
-      version: "v.1.6",
+      version: "v.1.7",
     });
   } catch (error) {
     console.log(error);
